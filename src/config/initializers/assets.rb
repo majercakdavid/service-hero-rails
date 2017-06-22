b739f19dfce181f3b5297ef402f3045c2922aaf4
David Majercak <david1majercak@gmail.com>
2017-06-22 23:36:20 +0200

    Rebuilt assets, added Hotjar

--- src/config/initializers/assets.rb
+++ src/config/initializers/assets.rb
@@ -19,6 +19,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Add folder with webpack generated assets to assets.paths
 Rails.application.config.assets.paths
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf )
 Rails.application.config.assets.precompile += %w( autocomplete-rails.js )
